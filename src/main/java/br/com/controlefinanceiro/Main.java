9df8fe12b56016515cc683261202d18e41ac516f
carloslsilva01 <carlos.lsilva@sptech.school>
2026-09-17 10:58:16 -0300

    Versão Beta: Classe Main menu e funcionalidades funcionando

--- src/main/java/br/com/controlefinanceiro/Main.java
+++ src/main/java/br/com/controlefinanceiro/Main.java
@@ -1,9 +1,11 @@
 package br.com.controlefinanceiro;
 
 import br.com.controlefinanceiro.model.Categoria;
+import br.com.controlefinanceiro.model.Movimentacao;
 import br.com.controlefinanceiro.service.FinanceiroService;
 
 import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +16,7 @@ public class Main {
         FinanceiroService servico = new FinanceiroService();
 
         while (true) {
-            System.out.println("===== CONTROLE FINANCEIRO =====");
+            System.out.println("\n===== CONTROLE FINANCEIRO =====");
             System.out.println("1. Adicionar receita");
             System.out.println("2. Adicionar despesa");
             System.out.println("3. Listar movimentações");
@@ -26,19 +28,81 @@ public class Main {
 
             switch (opcao) {
                 case 1:
-                    System.out.println("===== ADICIONAR RECEITA =====");
+                    System.out.println("\n===== ADICIONAR RECEITA =====");
                     System.out.println("Insira a descrição:");
                     String descricao = scTexto.nextLine();
-                    System.out.print("Insira o valor: ");
+                    System.out.print("Insira o valor (R$): ");
                     Double valor = scNumero.nextDouble();
                     System.out.print("Insira a categoria: ");
                     String categoria = scTexto.nextLine();
                     Boolean resultado = servico.adicionarReceita(descricao, valor, Categoria.valueOf(categoria));
                     if(resultado) {
-                        System.out.println("Certo!");
+                        System.out.println("Receita cadastrada com sucesso!");
                     } else {
-                        System.out.println("Erro!");
+                        System.out.println("Houve um erro ao cadastrar a receita!");
                     }
+                    break;
+                case 2:
+                    System.out.println("\n===== ADICIONAR DESPESA =====");
+                    System.out.println("Insira a descrição:");
+                    descricao = scTexto.nextLine();
+                    System.out.print("Insira o valor: ");
+                    valor = scNumero.nextDouble();
+                    System.out.print("Insira a categoria: ");
+                    categoria = scTexto.nextLine();
+                    resultado = servico.adicionarDespesa(descricao, valor, Categoria.valueOf(categoria));
+                    if(resultado) {
+                        System.out.println("Despesa cadastrada com sucesso!");
+                    } else {
+                        System.out.println("Houve um erro ao cadastrar a despesa!");
+                    }
+                    break;
+                case 3:
+                    System.out.println("\n===== MOVIMENTAÇÕES =====");
+                    List<Movimentacao> movimentacoes = servico.listarMovimentacoes();
+                    for (int i = 0; i < movimentacoes.size(); i++) {
+                        System.out.println("\n#" + (i + 1) + " - Movimentação");
+                        System.out.println("ID: " + movimentacoes.get(i).getId());
+                        System.out.println("Data: " + movimentacoes.get(i).getData());
+                        System.out.println("Valor (R$): " + movimentacoes.get(i).getValor());
+                        System.out.println("Categoria: " + movimentacoes.get(i).getCategoria());
+                        System.out.println("Descrição: " + movimentacoes.get(i).getDescricao());
+                        System.out.println("\n========================");
+                    }
+                    break;
+                case 4:
+                    System.out.println("\n===== SALDO =====");
+                    Double saldo = servico.consultarSaldo();
+                    System.out.printf("\nSaldo: R$ %.2f", saldo, "\n");
+                    break;
+                case 5:
+                    System.out.println("\n===== BUSCA POR CATEGORIA =====");
+                    System.out.print("Digite uma categoria: ");
+                    categoria = scTexto.next();
+                    Boolean valida = false;
+                    for(Categoria c: Categoria.values()) {
+                        if(c.equals(Categoria.valueOf(categoria))) {
+                            valida =  true;
+                            break;
+                        }
+                    }
+                    if(!valida) {
+                        System.out.println("Categoria Inexistente, tente novamente!");
+                        break;
+                    }
+                    movimentacoes = servico.buscarPorCategoria(Categoria.valueOf(categoria));
+                    for (int i = 0; i < movimentacoes.size(); i++) {
+                        System.out.println("\n#" + (i + 1) + " - Movimentação");
+                        System.out.println("ID: " + movimentacoes.get(i).getId());
+                        System.out.println("Data: " + movimentacoes.get(i).getData());
+                        System.out.println("Valor: " + movimentacoes.get(i).getValor());
+                        System.out.println("Categoria: " + movimentacoes.get(i).getCategoria());
+                        System.out.println("Descrição: " + movimentacoes.get(i).getDescricao());
+                        System.out.println("\n========================");
+                    }
+                    break;
+                case 0:
+                    System.exit(0);
             }
         }
     }
